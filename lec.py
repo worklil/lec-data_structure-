4e25224c8a7fe7cffdfec98748bc30e85e68dd8a
workliliia <88007852+worklil@users.noreply.github.com>
2023-08-01 23:57:14 +0100

    Add files via upload

---
+++ lec.py
@@ -0,0 +1,27 @@
+# delivery_types = ("Next Day", "3-4 Day", "Relaxed(7 day)")
+# menu_id = 0
+# print("Choose the delivery type you would like:")
+# for item in delivery_types:
+#     print(menu_id, item)
+#     menu_id = menu_id + 1
+# customer_delivery_type = int(input("Please choose your delivery type"))
+# print("You have chosen", delivery_types[customer_delivery_type])
+#---------------------------------------------------------------
+# week_days = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+# weekend_days = ("Saturday", "Sunday") # immutable tuple
+#
+# whole_week = week_days + weekend_days
+#
+# # for item in whole_week:
+# #     print(item)
+#
+# for item in whole_week[5:7]:
+#     print(item)
+#---------------------------------------------------------------
+
+#dictionary
+
+eng_ita = {}
+eng_ita["apple"] = "mela"
+eng_ita["orange"] = "mela"
+eng_ita["apple"] = "mela"
